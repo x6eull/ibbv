99ed9cc52763c5b7c13e11a84fbcd5903d2a9961
x6eull <x6eull@gmail.com>
2025-08-20 21:58:58 +0800

    Init benchmark framework

---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+#include <benchmark/benchmark.h>
+
+static void BM_SomeFunction(benchmark::State &state) {
+  // Perform setup here
+  for (auto _ : state) {
+    // This code gets timed
+    volatile auto b = 1 + 1;
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_SomeFunction);
+// Run the benchmark
+BENCHMARK_MAIN();
