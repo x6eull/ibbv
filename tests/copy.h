6b9b423b5b594b24eafae3bb05d2040ba9cdd1dd
x6eull <x6eull@gmail.com>
2025-08-26 13:55:25 +0800

    avoid random in BM_copy

--- tests/copy.h
+++ tests/copy.h
@@ -3,11 +3,11 @@
 
 namespace ibbv::test {
 template <typename T> static void BM_copy(benchmark::State &state) {
-  const auto max_num_blocks = state.range(0), occupied_percent = state.range(1);
-  const auto max_ele_idx = BlockSize * max_num_blocks - 1,
-             num_ele = BlockSize * max_num_blocks * occupied_percent / 100;
-  const auto rep = stable_random_dist(num_ele, max_ele_idx);
-  const auto bv_base = from_common<T>(rep);
+  const auto num_blocks = state.range(0), ele_per_block = state.range(1);
+  T bv_base{};
+  for (int i = 0; i < num_blocks; i++)
+    for (int j = 0; j < ele_per_block; j++)
+      bv_base.set(i * BlockSize + j);
   for (auto _ : state) {
     auto copy = bv_base;
     benchmark::DoNotOptimize(copy);
@@ -16,8 +16,8 @@ template <typename T> static void BM_copy(benchmark::State &state) {
 }
 
 static const inline auto copy_args = {
-    concat_vec({{0}, benchmark::CreateRange(1, 1 << 16, 2)}),
-    benchmark::CreateDenseRange(10, 90, 20),
+    concat_vec({{0}, benchmark::CreateRange(1, 1 << 16, 8)}),
+    benchmark::CreateRange(1, 128, 8),
 };
 BENCHMARK(BM_copy<IBBV>)->ArgsProduct(copy_args);
 BENCHMARK(BM_copy<SBV>)->ArgsProduct(copy_args);
