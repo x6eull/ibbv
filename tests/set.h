0a4761527de0ad94c5043e60b2eacff467e21895
x6eull <x6eull@gmail.com>
2025-08-28 18:27:09 +0800

    Add random set test

--- tests/set.h
+++ tests/set.h
@@ -13,11 +13,29 @@ template <typename T> static void BM_set_seq(benchmark::State &state) {
   }
   state.SetItemsProcessed(max_num_blocks * ele_per_block * state.iterations());
 }
-
 static const inline auto set_seq_args = {
     benchmark::CreateRange(1, 1LL << 24, 16),
     concat_vec({{1}, benchmark::CreateRange(2, 128, 4)}),
 };
 BENCHMARK(BM_set_seq<IBBV>)->ArgsProduct(set_seq_args);
 BENCHMARK(BM_set_seq<SBV>)->ArgsProduct(set_seq_args);
+
+template <typename T> static void BM_set_random(benchmark::State &state) {
+  const auto max_num_blocks = state.range(0), occupied_percent = state.range(1);
+  const auto max_ele_idx = BlockSize * max_num_blocks - 1,
+             num_ele = BlockSize * max_num_blocks * occupied_percent / 100;
+  const auto eles = stable_random_dist(num_ele, max_ele_idx);
+  for (auto _ : state) {
+    T vec{};
+    for (const auto b : eles)
+      vec.set(b);
+  }
+  state.SetItemsProcessed(eles.size() * state.iterations());
+}
+static const inline auto set_random_args = {
+    benchmark::CreateRange(1, 6400, 8),
+    benchmark::CreateDenseRange(10, 90, 20),
+};
+BENCHMARK(BM_set_random<IBBV>)->ArgsProduct(set_random_args);
+BENCHMARK(BM_set_random<SBV>)->ArgsProduct(set_random_args);
 } // namespace ibbv::test
