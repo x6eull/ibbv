ed8c1fab26d4b01c099e09190ae6b2669ce11004
x6eull <x6eull@gmail.com>
2025-09-14 18:29:46 +0800

    Implement RoaringBitVector

--- include/RoaringBitVector.h
+++ include/RoaringBitVector.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "roaring.c"
 #include "roaring.hh"
 
 namespace ibbv {
@@ -8,5 +9,74 @@ protected:
   roaring::Roaring rep;
 
 public:
+  // All constructors & destructors & assignments are default
+  RoaringBitVector() = default;
+  RoaringBitVector(roaring::Roaring&& r) : rep{std::move(r)} {}
+  using index_t = uint32_t;
+  using iterator = roaring::Roaring::const_iterator;
+
+  auto begin() const noexcept { return rep.begin(); }
+  auto end() const noexcept { return rep.end(); }
+  auto find_first() const noexcept { return rep.minimum(); }
+  auto empty() const noexcept { return rep.isEmpty(); }
+  auto count() const noexcept { return rep.cardinality(); }
+  void clear() noexcept { rep.clear(); }
+
+  auto test(index_t n) const noexcept { return rep.contains(n); }
+  auto reset(index_t n) noexcept { return rep.remove(n); }
+  auto set(index_t n) noexcept { return rep.add(n); }
+  auto test_and_set(index_t n) noexcept {
+    if (test(n)) return false;
+    set(n);
+    return true;
+  }
+
+  auto contains(const RoaringBitVector& rhs) const noexcept {
+    return rhs.rep.isSubset(rep);
+  }
+  auto intersects(const RoaringBitVector& rhs) const noexcept {
+    return rep.and_cardinality(rhs.rep) != 0;
+  }
+
+  auto operator==(const RoaringBitVector& rhs) const noexcept {
+    return rep == rhs.rep;
+  }
+  auto operator!=(const RoaringBitVector& rhs) const noexcept {
+    return !(*this == rhs);
+  }
+
+  bool operator|=(const RoaringBitVector& rhs) noexcept {
+    const auto old_count = count();
+    rep |= rhs.rep;
+    return count() != old_count;
+  }
+  RoaringBitVector operator|(const RoaringBitVector& rhs) const noexcept {
+    return RoaringBitVector{rep | rhs.rep};
+  }
+
+  bool operator&=(const RoaringBitVector& rhs) noexcept {
+    const auto old_count = count();
+    rep &= rhs.rep;
+    return count() != old_count;
+  }
+  RoaringBitVector operator&(const RoaringBitVector& rhs) const noexcept {
+    return RoaringBitVector{rep & rhs.rep};
+  }
+
+  bool operator-=(const RoaringBitVector& rhs) noexcept {
+    const auto old_count = count();
+    rep -= rhs.rep;
+    return count() != old_count;
+  }
+  RoaringBitVector operator-(const RoaringBitVector& rhs) const noexcept {
+    return RoaringBitVector{rep - rhs.rep};
+  }
+  bool intersectWithComplement(const RoaringBitVector& rhs) noexcept {
+    return *this -= rhs;
+  }
+  void intersectWithComplement(const RoaringBitVector& lhs,
+                               const RoaringBitVector& rhs) noexcept {
+    rep = lhs.rep - rhs.rep;
+  }
 };
 } // namespace ibbv
